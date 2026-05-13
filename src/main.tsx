2ade2816dcafcf55d65b1ee171baccfa1f8cc767
jorge-573 <jr.gonzalez07@outlook.com>
2026-05-13 00:45:36 -0500

    Fix build eror

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { ThemeProvider } from 'styled-components'
 import App from './App.tsx'
 import { GlobalStyles } from './styles/GlobalStyles'
-import { appTheme } from './styles/theme'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider theme={appTheme}>
-      <GlobalStyles />
-      <App />
-    </ThemeProvider>
+    <GlobalStyles />
+    <App />
   </StrictMode>
 )
